f9ae6252f561ca078d6c99797be766c58bfd86e2
Alban Peignier <alban@tryphon.eu>
2011-09-13 09:45:48 +0200

    Fixes OSM::ActiveRecord connection configuration

--- app/models/osm/active_record.rb
+++ app/models/osm/active_record.rb
@@ -2,11 +2,8 @@ require 'postgis_adapter'
 
 class OSM::ActiveRecord < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection({
-    :adapter => "postgresql",
-    :database => "osm",
-    :username => "osm",
-    :password => "osm",
-    :host => "osm.dryade.priv"
-  })
+
+  # establish_connection :osm doesn't work :(
+  establish_connection Rails.configuration.database_configuration["osm"]
+
 end
--- lib/active_osm.rb
+++ lib/active_osm.rb
@@ -2,8 +2,7 @@ module ActiveOSM
   class Engine < Rails::Engine
     initializer "initialize connection to OSM" do
       config.after_initialize do
-        # establish_connection :osm (here or into OSM::ActiveRecord) doesn't work with 3.0.7
-        OSM::ActiveRecord.establish_connection Rails.configuration.database_configuration["osm"]
+        
       end
     end
   end
